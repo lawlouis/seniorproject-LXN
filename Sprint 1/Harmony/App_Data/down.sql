f7f7fa6bf55ef9fd1607fb32aaeab0cab2e2a927
Penny <xyang16@wou.edu>
2020-06-03 04:21:21 -0700

    fix merge conflict form up.sql

--- Sprint 1/Harmony/App_Data/down.sql
+++ Sprint 1/Harmony/App_Data/down.sql
@@ -2,6 +2,14 @@
 -- #       Drop All Identity Tables      #
 -- #######################################
 
+ALTER TABLE [dbo].[AspNetUserClaims]  DROP CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] 	
+
+ALTER TABLE [dbo].[AspNetUserLogins]  DROP CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] 	
+
+ALTER TABLE [dbo].[AspNetUserRoles]  DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]	
+
+ALTER TABLE [dbo].[AspNetUserRoles]  DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId]
+
 DROP TABLE [dbo].[AspNetUserRoles]
 
 DROP TABLE [dbo].[AspNetRoles]
